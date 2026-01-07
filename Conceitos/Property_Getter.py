ae38bc343b25ce446304c336a35e5672dcaaf61c
Fabio Cesar Reis de Carvalho <53885395+fareis2000@users.noreply.github.com>
2026-01-07 12:53:26 -0300

    Add files via upload

---
+++ Conceitos/Property_Getter.py
@@ -0,0 +1,23 @@
+class Caneta:
+    def __init__(self, cor):
+        self.cor_tinta = cor
+    
+    @property
+    def cor(self):
+        return self.cor_tinta
+
+
+caneta = Caneta('Azul')
+print(caneta.cor)
+
+
+
+# class Caneta:
+#     def __init__(self, cor):
+#         self.cor = cor
+    
+#     def get_cor(self):
+#         return self.cor
+
+# caneta = Caneta('Azul')
+# print(caneta.get_cor())
