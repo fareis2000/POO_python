156f06616da7488ccb960e1d92ab92df3f2f59d4
fareis2000 <fareis2000@hotmail.com>
2026-05-29 16:02:44 -0300

    Adicionando a classe Ponto com métodos especiais __str__ e __repr__

--- Dunder/Methods.py
+++ Dunder/Methods.py
@@ -15,3 +15,24 @@
 # __neg__(self) - -self
 # __str__(self) - str
 # __repr__(self) - str
+
+class Ponto:
+    def __init__(self,x,y,z = 'str'):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+
+
+    def __str__(self):
+        return f'({self.x!r}, {self.y!r}, {self.z!r})'
+    
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'Ponto({self.x!r}, {self.y!r}, {self.z!r})'
+    
+p1 = Ponto(1,2)
+p2 = Ponto(3,4)
+
+print(p1)
+print(repr(p2))
