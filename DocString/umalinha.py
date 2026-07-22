8b22d88308105c19a7efdc823c8a22556c962489
fareis2000 <fareis2000@hotmail.com>
2026-07-22 11:00:20 -0300

    Atualização do projeto

---
+++ DocString/umalinha.py
@@ -0,0 +1,8 @@
+"""Documentação do módulo umalinha."""
+
+
+var = 'valor'
+
+def funcao():
+    """Esta é uma função de exemplo."""
+    return "Olá, mundo!"
---
+++ DocString/variaslinhas.py
@@ -0,0 +1,7 @@
+"""
+Documentação do módulo variaslinhas.py
+gere um texto longo de documentação para o módulo variaslinhas.py, com várias 
+linhas de texto, explicando o propósito do módulo, suas funcionalidades e como 
+utilizá-lo. Inclua exemplos de uso e detalhes sobre as funções e classes 
+presentes no módulo.
+"""
