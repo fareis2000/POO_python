a57ecd859319526b7db3fd108f2d995140c45198
fareis2000 <fareis2000@hotmail.com>
2026-07-22 09:54:27 -0300

    Atualização do projeto

--- Dunder/Exemplos.py
+++ Dunder/Exemplos.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Exemplo de uso de dunder methods (métodos mágicos)
 # __lt__(self,other) - self < other
 # __le__(self,other) - self <= other
@@ -38,4 +39,46 @@ if __name__ == '__main__':
     p3 = p1 + p2
     print(p3)
     print('P1 é maior que p2', p1 > p2)
+=======
+# Exemplo de uso de dunder methods (métodos mágicos)
+# __lt__(self,other) - self < other
+# __le__(self,other) - self <= other
+# __gt__(self,other) - self > other
+# __ge__(self,other) - self >= other
+# __eq__(self,other) - self == other
+# __ne__(self,other) - self != other
+# __add__(self,other) - self + other
+# __sub__(self,other) - self - other
+# __mul__(self,other) - self * other
+# __truediv__(self,other) - self / other
+# __neg__(self) - -self
+# __str__(self) - str
+# __repr__(self) - str
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return f'{class_name}(x={self.x!r}, y={self.y!r})'
+
+    def __add__(self, other):
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Ponto(novo_x, novo_y)
+
+    def __gt__(self, other):
+        resultado_self = self.x + self.y
+        resultado_other = other.x + other.y
+        return resultado_self > resultado_other
+
+
+if __name__ == '__main__':
+    p1 = Ponto(4, 2)  # 6
+    p2 = Ponto(6, 4)  # 10
+    p3 = p1 + p2
+    print(p3)
+    print('P1 é maior que p2', p1 > p2)
+>>>>>>> 0f11b0a (feat: add comprehensive Python OOP learning materials covering pillars, mixins, dunder methods, and exceptions)
     print('P2 é maior que p1', p2 > p1)
